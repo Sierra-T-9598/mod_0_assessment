86a4d18d6c7c31363dabb14cb9959476136b80cb
Sierra Tucker <sierra.tucker121@gmail.com>
2021-09-03 10:07:01 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Cheaper by the Dozen"
+is_entertaining = true
+movie = "Inception"
+num_4 = 4
+num_6 = 6
+product = num_6 * num_4
+favorite_foods = ["pizza", "ice cream", "yogurt"]
+favorite_foods.shift
+puts movie.length
