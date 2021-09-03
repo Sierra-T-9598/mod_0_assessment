2212289e9d8488cb5b382fee9768367c29c61632
Sierra Tucker <sierra.tucker121@gmail.com>
2021-09-03 10:35:31 -0600

    Create developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "first_name" => "Sierra",
+  "last_name" => "Tucker",
+  "age" => 23,
+  "experience_in_years" => 0,
+  "is_enjoying_Turing" => true,
+  "is_not_challenged" => false,
+  "strengths" => ["attention to detail", "perseverance", "internal locus of control"],
+  "weaknesses" => ["asking questions", "confidence", "not much experience"]
+}
