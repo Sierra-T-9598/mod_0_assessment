33142aff25e7edd7667eb09d559833a609bb1285
Sierra Tucker <sierra.tucker121@gmail.com>
2021-09-03 10:26:36 -0600

    Create dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "name" => "tyrannosaurus rex",
+  "name_meaning" => "king of the tyrant lizards",
+  "height_in_feet" => 13,
+  "weight_in_tons" => 9,
+  "is_strong" => true,
+  "is_fast" => true,
+  "trex_description" => ["carnivore", "serrated teeth", "200 bones"],
+  "diet" => ["herbivorous dinorsaurs", "Edmontosaurus", "Triceratops"]
+}
